17f77540c0152c75a9ba9ad94cc9872de4fe968e
Alexander Rafferty <alexander.drafferty@gmail.com>
2022-08-04 22:24:20 +0800

    Copied README to crate doc comment; v0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,22 @@
 #![warn(missing_docs)]
 
-//! This is a simple traffic microsimulation library with a focus on performance and realism.
+//! This is a simple traffic simulation module with a focus on performance and realism.
+//!
+//! This is a personal project, with currently poor documentation and is not quite feature complete.
+//!
+//! ## Goals
+//!
+//! The goal of this package is to be capable of simulating any road network in a realistic manner,
+//! with reasonable performance. This includes simulating things such as:
+//!
+//! * Car following
+//! * Lane-changing decisions and dynamics
+//! * Yielding at give way/stop signs
+//! * Merging and diverging
+//! * Simple path finding
+//!
+//! The primary gap right now is the lack of a lane-changing.
+//! There are pieces in place, but it is in general a very hard problem to solve.
 //!
 //! # Hello world
 //! This sample code demonstrates how to simulate a single vehicle on a straight, single-lane road.
